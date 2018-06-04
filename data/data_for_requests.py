84c5a6c3547649e29dd21322f8255d10113ecf8d
AndreiPisarev <berky.87@gmail.com>
2018-06-05 00:52:57 +0300

    Исправил часть замечаний

---
+++ data/data_for_requests.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+path_file = os.path.join(current_dir, 'config_file_git.json')
+
+with open(path_file) as file:
+    data = json.load(file)
+    TOKEN = data['TOKEN']
+    VERSION = data['VERSION']
+    ID = data['ID']
+
+
+params = {
+    'user_id': None,
+    'access_token': TOKEN,
+    'v': VERSION
+}
+
+params_group = {
+            'fields': ['members_count'],
+            'access_token': TOKEN,
+            'v': VERSION,
+            'group_id': None
+        }
--- thesis.py
+++ thesis.py
@@ -1,16 +1,24 @@
 import requests
 import json
-from Data.Data_for_requests import ID, params, params_group
+from data.data_for_requests import ID, params, params_group
+import time
 
 
 def retry(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
-        except:
-            print('Error during work {}, new attemp'.format(func))
-            raise
+
+        except RetryException:
+            time.sleep(1)
+            return wrapper(*args, **kwargs)
     return wrapper
+
+
+class RetryException(Exception):
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.errors = errors
 
 
 class VkReq():
@@ -20,20 +28,32 @@ class VkReq():
         состоят друзья 'target' """
         self.data_to_file = list()
         self.list_groups_friends = list()
-        self.list_friends_target = list()  # Добавил
-        self.id = id  # Добавил
-        self.list_groups_target = list()  # Добавил
-        self.merge_list_friends = list()
-        self.f = 0
-        self.g = 0
+        self.list_friends_target = list()
+        self.id = id
+        self.list_groups_target = list()
+
+
+    @retry
+    def requests_api(self, method, params):
+        """Функция принимает метод и параметры для запроса к API"""
+        time.sleep(0.34)
+        response = requests.get('https://api.vk.com/method/{}'.format(method), params=params)
+        response.raise_for_status()
+        response = response.json()
+        return response
+
 
     @retry
     def get_list_friends(self, id):
         """Функция принимает id пользователя и возврящает список его друзей"""
+
+        self.friends_counter = 0
+
         try:
-            params['user_id'] = id
-            response_friends = requests.get('https://api.vk.com/method/friends.get', params=params)
-            response_friends = response_friends.json()
+            method = 'friends.get'
+            params_request = params
+            params_request['user_id'] = id
+            response_friends = self.requests_api(method, params_request)
             list_friends = response_friends['response']['items']
             return list_friends
         except requests.exceptions.ReadTimeout:
@@ -43,12 +63,14 @@ class VkReq():
     def get_list_groups(self, id):
         """функция принимает id и возвращает список групп в которых состоит пользователь"""
         try:
-            params['user_id'] = id
-            print('Запрос информации по friend {}, осталось {}'.format(id, (len(vk.list_friends_target)-vk.f)))
-            response_group = requests.get('https://api.vk.com/method/groups.get', params=params)
-            response_group = response_group.json()
+            method = 'groups.get'
+            params_request = params
+            params_request['user_id'] = id
+            print('Запрос информации по другу с ID {}, осталось обработать друзей {}'.\
+                  format(id, len(self.list_friends_target) - self.friends_counter))
+            self.friends_counter += 1
+            response_group = self.requests_api(method, params_request)
             list_group = response_group['response']['items']
-            vk.f += 1
             return list_group  # По некоторым ID возвращает None, и попадает в список
 
         except KeyError:
@@ -59,6 +81,8 @@ class VkReq():
 
     def merge_list(self, list_friends):
         """Функция принимает список (вложенный), проходит циклом, проверяет элемент на None, и возврящает множество"""
+        self.merge_list_friends = list()
+
         for groups in list_friends:
             if groups is not None:
                 for group in groups:
@@ -69,15 +93,18 @@ class VkReq():
         """Функция принимает id групп и добавляет информацию в список data_to_file в виде словаря (id,
         название группы и количество участников в группе"""
         try:
+            method = 'groups.getById'
+            self.groups_counter = 0
             for group in groups:
-                params_group['group_id'] = group
-                print('Запрос информации по группе {}, осталось {}'.format(group, (len(groups) - vk.g)))
-                response = requests.get('https://api.vk.com/method/groups.getById', params=params_group)
-                response = response.json()
+                params_group_request = params_group
+                params_group_request['group_id'] = group
+                print('Запрос информации по группе c ID {}, осталось обработать групп {}'.format(group, (len(groups) -\
+                                                                                                self.groups_counter)))
+                self.groups_counter += 1
+                response = self.requests_api(method, params_group_request)
                 need_data = response['response'][0]
                 id_g, m_c, n_g = need_data['id'], need_data['members_count'], need_data['name']
                 self.data_to_file.append({'id': id_g, 'members_count': m_c, 'name': n_g})
-                vk.g +=1
             return self.data_to_file
         except requests.exceptions.ReadTimeout:
             raise
@@ -89,15 +116,12 @@ class VkReq():
             json.dump(data, groups_data, indent=2, ensure_ascii=False)
 
 
-vk = VkReq(ID)
-
-
 def main():
-
+    vk = VkReq(ID)
     vk.list_friends_target = vk.get_list_friends(vk.id)  # Получаем список друзей 'target'
     vk.list_groups_target = set(vk.get_list_groups(vk.id))  # Получаем список групп 'target' и преобразуем в множество
 
-    for friend in vk.list_friends_target:  # Циклом обходим всех друзей и получаем список групп всех его друзей
+    for friend in vk.list_friends_target[0:10]:  # Циклом обходим всех друзей и получаем список групп всех его друзей
         vk.list_groups_friends.append(vk.get_list_groups(friend))  # vk.list_groups_friends - состоит из вложенных спис.
 
     vk.list_groups_friends = vk.merge_list(vk.list_groups_friends)  # Преобразуем вложенный список в множество
